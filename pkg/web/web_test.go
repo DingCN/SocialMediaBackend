3e110af08b8cde328a8ba00e0f4908fbb85e1243
Hongyu Ding <hd1004@nyu.edu>
2018-11-18 02:00:12 -0500

    map storing pointer

--- pkg/web/web_test.go
+++ pkg/web/web_test.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -8,24 +9,53 @@ import (
 	"testing"
 )
 
+var addr = "//127.0.0.1:9090/createAccount.html"
+
 func Test_CreateAccount(t *testing.T) {
-
-	data := url.Values{}
-	data.Set("username", "asdf")
-	data.Add("password", "asdf")
-
-	req, err := http.NewRequest("POST", "//127.0.0.1:9090/createAccount.html", strings.NewReader(data.Encode()))
+	//var path = "/createAccount.html"
+	form := url.Values{}
+	form.Add("username", "asdf")
+	form.Add("password", "asdf")
+	//resp, err = http.PostForm(addr+path, form)
+	req, err := http.NewRequest("POST", addr, strings.NewReader(form.Encode()))
 	if err != nil {
 		t.Fatal(err)
 	}
-
+	req.PostForm = form
 	res := httptest.NewRecorder()
 
 	CreateAccount(res, req)
 
-	exp := "Hello World"
-	act := res.Body.String()
-	if exp != act {
-		t.Fatalf("Expected %s gog %s", exp, act)
+	var expected = "create account success"
+	var actual string
+	err = json.NewDecoder(res.Body).Decode(&actual)
+	if err != nil {
+		http.Error(res, err.Error(), 400)
+		return
+	}
+	if expected != actual {
+		t.Fatalf("Expected %s got %s", expected, actual)
 	}
 }
+
+// func Test_CreateAccount(t *testing.T) {
+
+// 	data := url.Values{}
+// 	data.Set("username", "asdf")
+// 	data.Add("password", "asdf")
+
+// 	req, err := http.NewRequest("POST", "//127.0.0.1:9090/createAccount.html", strings.NewReader(data.Encode()))
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	res := httptest.NewRecorder()
+
+// 	CreateAccount(res, req)
+
+// 	exp := "Hello World"
+// 	act := res.Body.String()
+// 	if exp != act {
+// 		t.Fatalf("Expected %s gog %s", exp, act)
+// 	}
+// }
